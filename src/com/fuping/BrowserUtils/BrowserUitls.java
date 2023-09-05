7ad349be143b4f177de34c7f28c42e1bc3c6649c
WINEZERO <kissloo@foxmail.com>
2023-09-06 06:20:23 +0800

    优化UI 增加CSS和XPATH选项

--- src/com/fuping/BrowserUtils/BrowserUitls.java
+++ src/com/fuping/BrowserUtils/BrowserUitls.java
@@ -1,21 +1,20 @@
 package com.fuping.BrowserUtils;
 
-import com.fuping.FXMLDocumentController;
 import com.teamdev.jxbrowser.chromium.Browser;
 import com.teamdev.jxbrowser.chromium.Cookie;
 import com.teamdev.jxbrowser.chromium.CustomProxyConfig;
 import com.teamdev.jxbrowser.chromium.dom.By;
+import com.teamdev.jxbrowser.chromium.dom.DOMDocument;
 import com.teamdev.jxbrowser.chromium.dom.internal.InputElement;
-import javafx.application.Platform;
 
 import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 import java.util.List;
 
-import static com.fuping.PrintLog.PrintLog.print_info;
-import static com.fuping.LoadConfig.MyConst.ClearCookiesSetting;
 import static com.fuping.LoadConfig.MyConst.BrowserProxySetting;
+import static com.fuping.LoadConfig.MyConst.ClearCookiesSetting;
+import static com.fuping.PrintLog.PrintLog.print_info;
 
 public class BrowserUitls {
 
@@ -53,5 +52,20 @@ public class BrowserUitls {
         }
     }
 
-
+    public InputElement findElementByAny(DOMDocument doc, String elementValue, boolean useId, boolean useName, boolean useClass, boolean useCss, boolean useXpath) {
+        //输入用户名元素 //需要添加输入XPath|CSS元素
+        InputElement userElement = null;
+        if (useId) {
+            userElement = (InputElement) doc.findElement(By.id(elementValue));
+        } else if (useName) {
+            userElement = (InputElement) doc.findElement(By.name(elementValue));
+        } else if (useClass){
+            userElement = (InputElement) doc.findElement(By.className(elementValue));
+        } if (useCss){
+            userElement = (InputElement) doc.findElement(By.cssSelector(elementValue));
+        } if (useXpath){
+            userElement = (InputElement) doc.findElement(By.xpath(elementValue));
+        }
+        return userElement;
+    }
 }
