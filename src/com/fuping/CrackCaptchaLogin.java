f097973fba975f360c1ec1694a8e5ee728348548
WINEZERO <kissloo@foxmail.com>
2023-09-07 17:18:52 +0800

    重命名

--- src/com/fuping/CrackCaptchaLogin.java
+++ src/com/fuping/CrackCaptchaLogin.java
@@ -27,13 +27,13 @@ public class CrackCaptchaLogin extends Application {
             e.setAccessible(true);
             Field f = ay.class.getDeclaredField("f");
             f.setAccessible(true);
-            Field modifersField = Field.class.getDeclaredField("modifiers");
-            modifersField.setAccessible(true);
-            modifersField.setInt(e, e.getModifiers() & ~Modifier.FINAL);
-            modifersField.setInt(f, f.getModifiers() & ~Modifier.FINAL);
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(e, e.getModifiers() & ~Modifier.FINAL);
+            modifiersField.setInt(f, f.getModifiers() & ~Modifier.FINAL);
             e.set(null, new BigInteger("1"));
             f.set(null, new BigInteger("1"));
-            modifersField.setAccessible(false);
+            modifiersField.setAccessible(false);
         } catch (Exception e1) {
             e1.printStackTrace();
         }
